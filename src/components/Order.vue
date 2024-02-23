58c3e161f29ec6fbe8382846cea4ba577fc31c88
Vova iz Tambova <voller.ruden@yandex.ru>
2024-02-23 21:17:14 +0300

    commint point

--- src/components/Order.vue
+++ src/components/Order.vue
@@ -1,72 +1,132 @@
-<script lang="ts"></script>
+<script lang="ts">
+export default {
+  data() {
+    return {
+      e1: 1,
+      steps: 1,
+      value: 1,
+    };
+  },
+
+  computed: {
+    disabled() {
+      return this.e1 === 1
+        ? "prev"
+        : this.e1 === this.steps
+        ? "next"
+        : undefined;
+    },
+  },
+  methods: {
+    nextStep(value: number) {
+      value++;
+    },
+  },
+};
+</script>
 
 <template>
-  <v-container style="border: 1px solid #e0e0e0" class="rounded">
-    <v-chip
-      size="x-large"
-      variant="text"
-      color="grey-darken-3"
-      style="font-weight: bold"
-    >
-      <v-icon
-        icon="mdi-file-chart-outline"
+  <div>
+    <v-container style="border: 1px solid #e0e0e0" class="rounded">
+      <v-chip
+        size="x-large"
+        variant="text"
+        color="grey-darken-3"
+        style="font-weight: bold"
+      >
+        <v-icon
+          icon="mdi-file-chart-outline"
+          color="blue-lighten-1"
+          class="mr-1"
+        ></v-icon>
+        Создание нового отчета
+      </v-chip>
+
+      <v-stepper hide-actions v-model="e1" editable bg-color="grey-lighten-2">
+        <template v-slot:default="{ prev, next }">
+          <v-stepper-header style="background-color: aqua;">
+            <v-stepper-item bg-color="black" edit-icon title="Шаблон" value="1"></v-stepper-item>
+            <v-divider></v-divider>
+
+            <v-stepper-item
+              edit-icon
+              title="Группировки"
+              value="2"
+            ></v-stepper-item>
+            <v-divider></v-divider>
+
+            <v-stepper-item
+              edit-icon
+              title="Показатели"
+              value="3"
+            ></v-stepper-item>
+            <v-divider></v-divider>
+
+            <v-stepper-item
+              edit-icon
+              title="Название и доступ"
+              value="4"
+            ></v-stepper-item>
+          </v-stepper-header>
+
+          <v-stepper-window>
+            <v-stepper-window-item value="1">
+              <v-card title=" One" flat>...</v-card>
+              <v-btn
+                style="border: 1px solid #bdbdbd"
+                class="ma-3 text-none"
+                color="blue-lighten-1"
+                variant="outlined"
+                @click="next"
+                >Продолжить</v-btn
+              >
+              <v-btn
+                style="border: 1px solid #bdbdbd"
+                class="text-none"
+                color="blue-lighten-1"
+                variant="outlined"
+                >Отменить</v-btn
+              >
+            </v-stepper-window-item>
+
+            <v-stepper-window-item value="2">
+              <v-card title=" Two" flat>...</v-card>
+            </v-stepper-window-item>
+
+            <v-stepper-window-item value="3">
+              <v-card title=" Three" flat>...</v-card>
+            </v-stepper-window-item>
+
+            <v-stepper-window-item value="4">
+              <v-card title=" Four" flat>...</v-card>
+            </v-stepper-window-item>
+          </v-stepper-window>
+          <v-row>
+            <v-stepper-actions
+              :disabled="disabled"
+              @click:prev="prev"
+              @click:next="next"
+            >
+            </v-stepper-actions>
+            <v-btn>Отмена</v-btn>
+          </v-row>
+        </template>
+      </v-stepper>
+
+      <v-btn
+        style="border: 1px solid #bdbdbd"
+        class="ma-3 text-none"
         color="blue-lighten-1"
-        class="mr-1"
-      ></v-icon>
-      Создание нового отчета
-    </v-chip>
-
-    <v-stepper flat hide-actions editable bg-color="grey-lighten-2">
-      <v-stepper-header>
-        <v-stepper-item edit-icon title="Шаблон" value="1"></v-stepper-item>
-        <v-divider></v-divider>
-        <v-stepper-item
-          edit-icon
-          title="Группировки"
-          value="2"
-        ></v-stepper-item>
-        <v-divider></v-divider>
-        <v-stepper-item edit-icon title="Показатели" value="3"></v-stepper-item>
-        <v-divider></v-divider>
-        <v-stepper-item
-          edit-icon
-          title="Название и доступ"
-          value="4"
-        ></v-stepper-item>
-      </v-stepper-header>
-
-      <v-stepper-window>
-        <v-stepper-window-item value="1">
-          <v-card title=" One" flat>...</v-card>
-        </v-stepper-window-item>
-
-        <v-stepper-window-item value="2">
-          <v-card title=" Two" flat>...</v-card>
-        </v-stepper-window-item>
-
-        <v-stepper-window-item value="3">
-          <v-card title=" Three" flat>...</v-card>
-        </v-stepper-window-item>
-
-        <v-stepper-window-item value="4">
-          <v-card title=" Four" flat>...</v-card>
-        </v-stepper-window-item>
-      </v-stepper-window>
-    </v-stepper>
-
-    <v-btn
-      style="border: 1px solid #bdbdbd"
-      class="ma-3 text-none"
-      color="blue-lighten-1"
-      variant="outlined"
-      >Продолжить</v-btn
-    >
-    <v-btn
-      style="border: 1px solid #bdbdbd"
-      class="text-none"
-      color="blue-lighten-1"
-      variant="outlined"
-      >Отменить</v-btn
-    >
-  </v-container>
+        variant="outlined"
+        >Продолжить</v-btn
+      >
+      <v-btn
+        style="border: 1px solid #bdbdbd"
+        class="text-none"
+        color="blue-lighten-1"
+        variant="outlined"
+        >Отменить</v-btn
+      >
+    </v-container>
+  </div>
 </template>
